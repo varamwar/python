cfb92a06346ec8c7aaa7b319abfe12d330f7ac4f
varamwar <88922829+varamwar@users.noreply.github.com>
2021-09-11 17:11:16 +0530

    Add files via upload

---
+++ print types.py
@@ -0,0 +1,194 @@
+'''print types'''
+# print("hello world")
+# print('hello world')
+# print('''hello world''')
+# print("""hello world""")
+# print("hello","world")
+# print("hello "+"world")
+# print('hello''world')
+# print("hello""world")
+# print("hello ""world")
+# print('hello '+'world ')
+# print("hello""world")
+# print("hello"" world")
+# print("""hello """ """world""")
+# print("""hello""" """ world""")
+# print("hello","world",sep=" ")
+
+
+
+
+'''end method'''
+
+# a="varam"
+# b="prasad"
+# print(a,b,end=".")
+# a="varaprasad"
+# b="ramatenki"
+# C="@gmail"
+# print(a,b,C,end=".com",sep="")
+
+# print(a)
+
+
+
+
+'''Separate method'''
+
+
+
+
+
+# a="varam"
+# b="war"
+# c="00"
+# print(a,b,c,sep=".")
+# print(a,b,c,sep="_")
+# print("hello","world",sep=" ")
+
+
+
+'''combination of end and sep'''
+
+
+# a="varaprasad"
+# b="ramatenki"
+# C="@gmail"
+# print(a,b,C,end=".com",sep="")
+
+'''Escape Sequences'''
+# print("hell \nworld")
+# print("varam\nwar")
+# print("varam\bwar")
+# print("varam \bwar")
+# print("varam  \bwar")
+# print("varam \twar")
+# print("varam\twar")
+# print("\tvaramwar")
+# print("vara\t mwar")
+# print("\u0001")
+# print("\u0002")
+# print("\u0003")
+# print("\u0004")
+# print("\u0005")
+# print("\u0006")
+# print("\U0001f601")
+# print("\U0001f602")
+# print("\U0001f603")
+# print("\U0001f604")
+# print("\U0001f605")
+# print("\U0001f606")
+# print("\U0001f611")
+# print("kjhfkjsdfhkdhfkfhwk\
+# iohfiwfhweifhw")
+# print("ramatenki\'varaprasad")
+# print("ramatenki\"varaprasad")
+# print("ramatenki\\varaprasad")
+# print("C:\\Users\\Josh\\Desktop\\incline\\inline>")
+
+
+
+'''Variables'''
+'''first way'''
+# a=1   
+# A=2
+# _b=3
+# _1=4
+# c_a=5
+# v_2=464
+# # print(a)
+# # print(_b)
+# # print(_1)
+# # print(c_a)
+# # print(v_2)
+# # print(a,_1,v_2,sep="@"
+
+'''second way'''
+# print(a),print(A),print(_b),print(c_a)
+# a=12;b=46;_1=211
+# print(a,b,_1)
+
+'''third way'''
+# a,b,c,d=1,2,3,4
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(a,b,c,d)
+'''fourth way'''
+# a=b=2
+# print(a)
+# print(b)
+# print(c,d,e)
+
+'''Format specifier'''
+'''%d'''
+# a=5  #int,%d,%i
+# b=5.5  #float %f
+# c="varam"  #string %s
+# d=555     # int %d,%i
+# print("the value of a is %d"%a)
+# print("the value of b is %f"%a)
+# e=print("the value of d is %s"%d)
+# e=print("the value of d is %s"%d)
+# print(type(e))
+
+
+# war='varam'
+# print(war[0]) #v
+# print(war[1]) #a
+# print(war[2]) #r
+# print(war[3]) #a
+# print(war[4]) #m
+# print(war[5])  #string index out of range
+
+'''Type conversion'''
+
+# a=5
+# print(a,type(a)) #5 <class 'int'>
+# b=float(a)
+# print(b,type(b)) #5.0 <class "float">
+# war=str(a)
+# print(war,type(war))
+
+
+# c=5.0
+# print(c,type(c))
+# v=int(c)
+# print(v,type(v))
+# m=str(c)
+# print(m,type(m))
+
+
+# a="varam"
+# print(a,type(a))
+# b=int(a)
+# print(b,type(b))
+# c=float(a)
+# print(c,type(c))
+
+
+# a="123456"
+# print(a,type(a))
+# b=int(a)
+# print(b,type(b))
+# c=float(a)
+# print(c,type(c))
+
+
+
+a="555.00"
+print(a,type(a))
+# b=int(a)
+# print(b,type(b))
+c=float(a)
+print(c,type(c))
+d=int(c)
+print(d,type(d))
+
+
+
+
+
+
+
